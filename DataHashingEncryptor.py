e69ed80ac798c1a9c7e0e93511875bc193ad270d
Jp Mendoza <jeanpauljr.mendoza@gmail.com>
2023-10-22 21:59:14 +0800

    Updated datahashingencryptor

--- DataHashingEncryptor.py
+++ DataHashingEncryptor.py
@@ -3,10 +3,79 @@ from cryptography.fernet import Fernet
 from argon2 import PasswordHasher, exceptions
 import os
 import hashlib
-
+import uuid
 
 blockchain = []
 
+
+# REGISTERING
+# CREATE FUNCTION THAT WILL SALT DATA
+def generate_salt():
+    fresh_salt = os.urandom(16)
+    return fresh_salt
+
+def hash_password(password, salt):
+    # add password and salt together
+    combined_pass = password + salt
+
+    # start hasher
+    ph = PasswordHasher()
+
+    # argon hash and sha256
+    argon_hashed = ph.hash(combined_pass).encode("utf-8")
+    hashed_pass = hashlib.sha256(argon_hashed).hexdigest()
+
+    return hashed_pass # returns string
+
+def add_to_database(dbname, data):
+    # data will be hashed
+    # added to database
+
+
+def delete_in_database(dbname, data):
+    #delete in database
+def edit_in_database(dbname, data):
+
+def return_data_in_database(dbname, query):
+
+
+
+
+
+
+# USERDATA table
+#   data ID
+#   data string
+#   salt byte
+#   hash string
+#   previous hash string
+#FK userID int
+
+# USERINFO Table
+#   UserID int
+#   Username string
+#   UserData array(?)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# CREATE FUNCTION THAT WILL DECRYPT
 
 # ENCRYPT
 # SALT  + ENCRYPTED
@@ -26,7 +95,9 @@ cipher = Fernet(fernet_key)
 encrypted_password = cipher.encrypt(input_pass.encode())
 
 print(encrypted_password)
-print(b'gAAAAABlNRZfI3H-FzWHOp8VhcpzNsTMPlL3QQu9jOH_u3Utn5FFRJvp_3LJlMcP4zYYnpEwLBOWDCGJjYqGyvTdfpbj16ClJw==')
+
+decrypted = cipher.decrypt(encrypted_password)
+print(decrypted.decode())
 
 # ADD SALT
 # generate a SALT then place it in a private blockchain
@@ -36,7 +107,6 @@ print(b'gAAAAABlNRZfI3H-FzWHOp8VhcpzNsTMPlL3QQu9jOH_u3Utn5FFRJvp_3LJlMcP4zYYnpEw
 salt = b'\x85\x8a$\xee\x9a-!M\x84\xfa\x7frc\x99\x82n'
 combined_data = encrypted_password + salt
 
-
 # ARGON HASH
 # instantiate PasswordHasher() as ph
 # use ph to argon hash the combined salt and encrypted password
@@ -44,8 +114,6 @@ combined_data = encrypted_password + salt
 
 ph = PasswordHasher()
 argon_hashed_data = ph.hash(combined_data).encode("utf-8")
-
-
 
 # HASH 256
 # take the argon hash and hash it using SHA 256
@@ -58,13 +126,3 @@ if final_hash == stored_pass:
     print("Passwords match.")
 else:
     print("Passwords do not match.")
-
-
-
-
-
-
-
-
-
-
--- main.py
+++ main.py
@@ -6,52 +6,8 @@ import os
 
 blockchain = []
 
-# Generate a Fernet key (encryption key)
-#fernet_key = Fernet.generate_key()
-fernet_key = b'uOv6bFZmI3IrR8r6CB0N3lYK66LVA2Vt66lKsHfjpD8='
-#salt = os.urandom(16)  # Generate a random salt
-salt = b'\x85\x8a$\xee\x9a-!M\x84\xfa\x7frc\x99\x82n'
 
-# Create an Argon2 password hasher
-ph = PasswordHasher()
-
-# Store this securely in your database
-stored_hashed_password = '$argon2id$v=19$m=65536,t=3,p=4$0mVnvqgGd04KjB7FLzqttg$wD5h2I5rVFHiW6jHL4c7/+KO25M0Zm7pXw6EKsLOTiY'
-
-# User input password
-user_input_password = input("Please input password: ")
-
-# Encrypt the password using Fernet
-cipher = Fernet(fernet_key)
-encrypted_password = cipher.encrypt(user_input_password.encode())
-
-# Combine the salt and the password
-combined_data = salt + encrypted_password
-
-# Hash the combined data using Argon2
-hashed_combined_data = ph.hash(combined_data).encode("utf-8")
-
-print(fernet_key)
-print("Stored pass: " + stored_hashed_password)
-# print("Final hash: " + hashed_combined_data)
-
-# Check if the input password matches the stored password
-try:
-    # Retrieve the salt and stored hashed password from your database
-    # In this example, we're using the salt and stored_hashed_password generated earlier
-
-    # Combine the salt and the input password
-    # combined_data = salt + cipher.encrypt(user_input_password.encode())
-
-    # Verify the combined data against the stored hashed password
-    ph.verify(hashed_combined_data, hashed_combined_data)
-    print("Password is correct.")
-except exceptions.VerifyMismatchError:
-    print("Password is incorrect. Old Password is now Overwritten with new")
+testblock = Block("First hash",['Username: Nico','Password'])
+print(testblock.block_hash)
 
 
-
-
-# encrypt
-# combine salt then hash
-# verify
